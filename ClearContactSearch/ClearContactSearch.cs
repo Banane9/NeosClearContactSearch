50dff07b800d1f52ac1bb34c934630952fbdfdfd
Arne Kiesewetter <banane9@live.de>
2022-11-19 18:30:39 +0100

    Add project files.

---
+++ ClearContactSearch/ClearContactSearch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using BaseX;
+using CodeX;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using FrooxEngine.LogiX.Data;
+using FrooxEngine.LogiX.ProgramFlow;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using NeosModLoader;
+
+namespace ClearContactSearch
+{
+    public class ClearContactSearch : NeosMod
+    {
+        public override string Author => "Banane9";
+        public override string Link => "https://github.com/Banane9/NeosClearContactSearch";
+        public override string Name => "ClearContactSearch";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony($"{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(FriendsDialog), "OnAttach")]
+        private static class SearchBarPatch
+        {
+            [HarmonyPostfix]
+            private static void OnAttachPostfix(SyncRef<TextField> ____searchBar)
+            {
+                var searchBar = ____searchBar.Target.Slot;
+
+                var builder = new UIBuilder(searchBar.Parent);
+                builder.CurrentRect.OffsetMin.Value += new float2(2, 0);
+                builder.CurrentRect.OffsetMax.Value += new float2(-2, 0);
+
+                builder.VerticalFooter(32, out var footer, out var content);
+
+                searchBar.Parent = content.Slot;
+                content.OffsetMax.Value += new float2(-2, 0);
+
+                builder = new UIBuilder(footer);
+                builder.Button("∅").LocalPressed += (sender, args) =>
+                {
+                    ____searchBar.Target.Editor.Target.Text.Target.Text = "";
+                    ____searchBar.Target.Editor.Target.ForceEditingChangedEvent();
+                };
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Clear Contact Search
+===========================
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that adds a clear button to the search bar on the contacts panel in the dash.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [ClearContactSearch.dll](https://github.com/Banane9/NeosClearContactSearch/releases/latest/download/ClearContactSearch.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
